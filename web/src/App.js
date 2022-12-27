eb59e899b5c951b22a3eb8be3aefb6cf2b01afcb
Grant Folgate <gfolgate@outlook.com>
2022-12-27 16:33:41 -0500

    remove exact path reduncancies

--- web/src/App.js
+++ web/src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Router>
     <NavBarElement />
     <Routes>
-        <Route exact path='/' exact element={<Home />} />
+        <Route path='/' element={<Home />} />
         <Route path='/about' element={<About/>} />
         <Route path='/contact' element={<Contact/>} />
     </Routes>
