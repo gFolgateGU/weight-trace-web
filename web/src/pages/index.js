499e1087249e8d24d8d32c9f21c5e96b279e4cc2
Grant Folgate <gfolgate@outlook.com>
2024-04-25 22:47:40 -0400

    rename home page

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1>ENV VIEW Web (Home Page)</h1>
+      <h1>Strava Calendar Lite (Home Page)</h1>
     </div>
   );
 };
