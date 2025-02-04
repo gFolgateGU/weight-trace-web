c754e03ed5c83e7f5784482dac603c506c4913b1
Grant Folgate <gfolgate@outlook.com>
2025-02-03 21:23:32 -0500

    add summary table

--- web/src/components/Profile.js
+++ web/src/components/Profile.js
@@ -1,21 +1,21 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
-import { Doughnut } from 'react-chartjs-2'
-import '../css/Profile.css'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
+import '../css/Profile.css';
 
-ChartJS.register(ArcElement, Tooltip, Legend)
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Profile = ({ token }) => {
     const [firstname, setFirstName] = useState('');
     const [lastname, setLastName] = useState('');
     const [city, setCity] = useState('');
-    const [state, setState] = useState('')
+    const [state, setState] = useState('');
     const [imagePath, setImagePath] = useState('');
     const [activityStats, setActivityStats] = useState({
-        all_run_totals: { distance: 0 },
-        all_ride_totals: { distance: 0 },
-        all_swim_totals: { distance: 0 }
+        all_run_totals: { distance: 0, count: 0, moving_time: 0, elapsed_time: 0, elevation_gain: 0 },
+        all_ride_totals: { distance: 0, count: 0, moving_time: 0, elapsed_time: 0, elevation_gain: 0 },
+        all_swim_totals: { distance: 0, count: 0, moving_time: 0, elapsed_time: 0, elevation_gain: 0 },
     });
 
     const data = {
@@ -24,9 +24,9 @@ const Profile = ({ token }) => {
             {
                 label: 'Percentage ( % ) of Training',
                 data: [
-                    activityStats.all_run_totals.distance / (activityStats.all_run_totals.distance + activityStats.all_ride_totals.distance + activityStats.all_swim_totals.distance),
-                    activityStats.all_ride_totals.distance / (activityStats.all_run_totals.distance + activityStats.all_ride_totals.distance + activityStats.all_swim_totals.distance),
-                    activityStats.all_swim_totals.distance / (activityStats.all_run_totals.distance + activityStats.all_ride_totals.distance + activityStats.all_swim_totals.distance),
+                    100.0 * (activityStats.all_run_totals.count / (activityStats.all_run_totals.count + activityStats.all_ride_totals.count + activityStats.all_swim_totals.count)),
+                    100.0 * (activityStats.all_ride_totals.count / (activityStats.all_run_totals.count + activityStats.all_ride_totals.count + activityStats.all_swim_totals.count)),
+                    100.0 * (activityStats.all_swim_totals.count / (activityStats.all_run_totals.count + activityStats.all_ride_totals.count + activityStats.all_swim_totals.count)),
                 ],
                 backgroundColor: [
                     '#000080',
@@ -48,19 +48,18 @@ const Profile = ({ token }) => {
         const fetchData = async () => {
             try {
                 const response = await axios.get('/api/athlete');
-                console.log(response.data)
+                console.log(response.data);
                 if (response.data) {
                     if ('error' in response.data) {
-                        console.log("Failed to load data for the athlete.")
-                        console.log(response.data)
-                    }
-                    else {
+                        console.log("Failed to load data for the athlete.");
+                        console.log(response.data);
+                    } else {
                         setFirstName(response.data["firstname"]);
                         setLastName(response.data["lastname"]);
                         setCity(response.data["city"]);
                         setState(response.data["state"]);
                         setImagePath(response.data["profile"]);
-                        setActivityStats(response.data["activity_stats"])
+                        setActivityStats(response.data["activity_stats"]);
                     }
                 }
                 if (!response.ok) {
@@ -72,29 +71,68 @@ const Profile = ({ token }) => {
         };
 
         fetchData();
-    });
+    }, []); // Empty array ensures useEffect runs only once when the component mounts
 
     return (
         <div>
-            <div class="profile-container">
-                <div class="black-div1"></div>
-                <div class="black-div"></div>
-                { <div>
-                    {imagePath && <img src={imagePath} class="profile-image" alt="Description of the image" />}
-                </div> }
-                <div class="profile-name">{firstname} {lastname}</div>
-                <div class="profile-name">{city}, {state}</div>
-            </div>
-            <div class="data-div">
-                <div class="graph-div">
-                    <Doughnut data={data} />
+            <div className="profile-container">
+                <div className="black-div1"></div>
+                <div className="black-div"></div>
+                <div>
+                    {imagePath && <img src={imagePath} className="profile-image" alt="Description of the image" />}
                 </div>
-                <div class="text-div">
+                <div className="profile-name">{firstname} {lastname}</div>
+                <div className="profile-name">{city}, {state}</div>
+            </div>
+            <div className="data-div">
+                <div className="summary-table">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Activity</th>
+                                <th>Total Distance (m)</th>
+                                <th>Count</th>
+                                <th>Moving Time (s)</th>
+                                <th>Elapsed Time (s)</th>
+                                <th>Elevation Gain (cm)</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Running</td>
+                                <td>{activityStats.all_run_totals.distance}</td>
+                                <td>{activityStats.all_run_totals.count}</td>
+                                <td>{activityStats.all_run_totals.moving_time}</td>
+                                <td>{activityStats.all_run_totals.elapsed_time}</td>
+                                <td>{activityStats.all_run_totals.elevation_gain}</td>
+                            </tr>
+                            <tr>
+                                <td>Cycling</td>
+                                <td>{activityStats.all_ride_totals.distance}</td>
+                                <td>{activityStats.all_ride_totals.count}</td>
+                                <td>{activityStats.all_ride_totals.moving_time}</td>
+                                <td>{activityStats.all_ride_totals.elapsed_time}</td>
+                                <td>{activityStats.all_ride_totals.elevation_gain}</td>
+                            </tr>
+                            <tr>
+                                <td>Swimming</td>
+                                <td>{activityStats.all_swim_totals.distance}</td>
+                                <td>{activityStats.all_swim_totals.count}</td>
+                                <td>{activityStats.all_swim_totals.moving_time}</td>
+                                <td>{activityStats.all_swim_totals.elapsed_time}</td>
+                                <td>{activityStats.all_swim_totals.elevation_gain}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                
+                <div className="graph-div">
+                    <Doughnut data={data} />
                 </div>
             </div>
         </div>
-
     );
+    
 };
 
 export default Profile;
